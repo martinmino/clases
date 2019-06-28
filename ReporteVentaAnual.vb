403719040ef460f10f89e4376e21e794ac5e7532
Martin Miño <mmino@paternal.matafuegosgeorgia.com>
2019-06-28 15:55:00 -0300

    Update ReporteVentaAnual.vb

--- ReporteVentaAnual.vb
+++ ReporteVentaAnual.vb
@@ -45,7 +45,8 @@ Public Class ReporteVentaAnual
             Case 2
                 ObtenerVentasExtintor()
 
-            Case Else
+            Case 3
+                ObtenerVentasSF()
 
         End Select
 
@@ -80,79 +81,120 @@ Public Class ReporteVentaAnual
             dr1.Delete()
         Next
 
-        If TipoReporte = 1 Then
-            'Recupero las familias 4 y 5
-            Sql = "SELECT DISTINCT ' ' AS tsicod_1, ' ' AS tsicod_2, tsicod_3, tsicod_4 "
-            Sql &= "FROM itmmaster "
-            Sql &= "WHERE tsicod_3 <> ' ' AND tsicod_4 <> ' ' "
-            Sql &= "ORDER BY tsicod_3, tsicod_4"
+        Select Case TipoReporte
+            Case 1
+                'Recupero las familias 4 y 5
+                Sql = "SELECT DISTINCT ' ' AS tsicod_1, ' ' AS tsicod_2, tsicod_3, tsicod_4 "
+                Sql &= "FROM itmmaster "
+                Sql &= "WHERE tsicod_3 <> ' ' AND tsicod_4 <> ' ' "
+                Sql &= "ORDER BY tsicod_3, tsicod_4"
 
-            da = New OracleDataAdapter(Sql, cn)
-            da.Fill(dt)
+                da = New OracleDataAdapter(Sql, cn)
+                da.Fill(dt)
 
-            'Agrego las familias 4 y 5 a la tabla
-            For Each dr In dt.Rows
-                dr1 = dtPpal.NewRow
-                dr1("usr_0") = Usr.Codigo
-                dr1("tsicod1_0") = " "
-                dr1("tsicod2_0") = " "
-                dr1("tsicod3_0") = dr("tsicod_3")
-                dr1("tsicod4_0") = dr("tsicod_4")
-                dr1("qtydia_0") = 0
-                dr1("predia_0") = 0
-                dr1("impdia_0") = 0
-                dr1("mardia_0") = 0
-                dr1("qtyacu_0") = 0
-                dr1("preacu_0") = 0
-                dr1("impacu_0") = 0
-                dr1("maracu_0") = 0
-                dr1("qtypro_0") = 0
-                dr1("imppro_0") = 0
-                dr1("marpro_0") = 0
-                dr1("qtyhis_0") = 0
-                dr1("prehis_0") = 0
-                dr1("imphis_0") = 0
-                dr1("fecha_0") = Hasta.AddDays(-1)
-                dtPpal.Rows.Add(dr1)
-            Next
-        Else
-            'Recupero las familias 4 y 5
-            Sql = " SELECT EXTRACT(MONTH FROM accdat_0) AS MES, itm.tsicod_4, itm.tsicod_2, itm.tsicod_1, ' ' AS cant  "
-            Sql &= "FROM (sinvoice siv INNER JOIN sinvoiced sid ON (siv.num_0 = sid.num_0)) INNER JOIN itmmaster itm ON (sid.itmref_0 = itm.itmref_0) "
-            Sql &= "WHERE EXTRACT(YEAR FROM accdat_0) = :accdat_0 AND sivtyp_0 <> 'PRF' AND tsicod_3 = '301' "
-            Sql &= "GROUP BY EXTRACT(MONTH FROM accdat_0), itm.tsicod_4, itm.tsicod_2, itm.tsicod_1 "
-            Sql &= "ORDER BY mes, tsicod_4, tsicod_2, tsicod_1"
+                'Agrego las familias 4 y 5 a la tabla
+                For Each dr In dt.Rows
+                    dr1 = dtPpal.NewRow
+                    dr1("usr_0") = usr.Codigo
+                    dr1("tsicod1_0") = " "
+                    dr1("tsicod2_0") = " "
+                    dr1("tsicod3_0") = dr("tsicod_3")
+                    dr1("tsicod4_0") = dr("tsicod_4")
+                    dr1("qtydia_0") = 0
+                    dr1("predia_0") = 0
+                    dr1("impdia_0") = 0
+                    dr1("mardia_0") = 0
+                    dr1("qtyacu_0") = 0
+                    dr1("preacu_0") = 0
+                    dr1("impacu_0") = 0
+                    dr1("maracu_0") = 0
+                    dr1("qtypro_0") = 0
+                    dr1("imppro_0") = 0
+                    dr1("marpro_0") = 0
+                    dr1("qtyhis_0") = 0
+                    dr1("prehis_0") = 0
+                    dr1("imphis_0") = 0
+                    dr1("fecha_0") = Hasta.AddDays(-1)
+                    dtPpal.Rows.Add(dr1)
+                Next
 
-            da = New OracleDataAdapter(Sql, cn)
-            da.SelectCommand.Parameters.Add("accdat_0", OracleType.Number).Value = AnoConsulta
-            da.Fill(dt)
+            Case 2
+                'Recupero las familias 4 y 5
+                Sql = " SELECT EXTRACT(MONTH FROM accdat_0) AS MES, itm.tsicod_4, itm.tsicod_2, itm.tsicod_1, ' ' AS cant  "
+                Sql &= "FROM (sinvoice siv INNER JOIN sinvoiced sid ON (siv.num_0 = sid.num_0)) INNER JOIN itmmaster itm ON (sid.itmref_0 = itm.itmref_0) "
+                Sql &= "WHERE EXTRACT(YEAR FROM accdat_0) = :accdat_0 AND sivtyp_0 <> 'PRF' AND tsicod_3 = '301' "
+                Sql &= "GROUP BY EXTRACT(MONTH FROM accdat_0), itm.tsicod_4, itm.tsicod_2, itm.tsicod_1 "
+                Sql &= "ORDER BY mes, tsicod_4, tsicod_2, tsicod_1"
 
-            'Agrego las familias 4 y 5 a la tabla
-            For Each dr In dt.Rows
-                dr1 = dtPpal.NewRow
-                dr1("usr_0") = usr.Codigo
-                dr1("tsicod1_0") = dr("tsicod_1")
-                dr1("tsicod2_0") = dr("tsicod_2")
-                dr1("tsicod3_0") = dr("mes")
-                dr1("tsicod4_0") = dr("tsicod_4")
-                dr1("qtydia_0") = 0
-                dr1("predia_0") = dr("mes")
-                dr1("impdia_0") = 0
-                dr1("mardia_0") = 0
-                dr1("qtyacu_0") = 0
-                dr1("preacu_0") = 0
-                dr1("impacu_0") = 0
-                dr1("maracu_0") = 0
-                dr1("qtypro_0") = 0
-                dr1("imppro_0") = 0
-                dr1("marpro_0") = 0
-                dr1("qtyhis_0") = 0
-                dr1("prehis_0") = 0
-                dr1("imphis_0") = 0
-                dr1("fecha_0") = Hasta.AddDays(-1)
-                dtPpal.Rows.Add(dr1)
-            Next
-        End If
+                da = New OracleDataAdapter(Sql, cn)
+                da.SelectCommand.Parameters.Add("accdat_0", OracleType.Number).Value = AnoConsulta
+                da.Fill(dt)
+
+                'Agrego las familias 4 y 5 a la tabla
+                For Each dr In dt.Rows
+                    dr1 = dtPpal.NewRow
+                    dr1("usr_0") = usr.Codigo
+                    dr1("tsicod1_0") = dr("tsicod_1")
+                    dr1("tsicod2_0") = dr("tsicod_2")
+                    dr1("tsicod3_0") = dr("mes")
+                    dr1("tsicod4_0") = dr("tsicod_4")
+                    dr1("qtydia_0") = 0
+                    dr1("predia_0") = dr("mes")
+                    dr1("impdia_0") = 0
+                    dr1("mardia_0") = 0
+                    dr1("qtyacu_0") = 0
+                    dr1("preacu_0") = 0
+                    dr1("impacu_0") = 0
+                    dr1("maracu_0") = 0
+                    dr1("qtypro_0") = 0
+                    dr1("imppro_0") = 0
+                    dr1("marpro_0") = 0
+                    dr1("qtyhis_0") = 0
+                    dr1("prehis_0") = 0
+                    dr1("imphis_0") = 0
+                    dr1("fecha_0") = Hasta.AddDays(-1)
+                    dtPpal.Rows.Add(dr1)
+                Next
+
+            Case 3
+                'Recupero las familias 2, 3, 4 y 5
+                Sql = "SELECT DISTINCT tsicod_1, tsicod_2, tsicod_3, tsicod_4 "
+                Sql &= "FROM itmmaster "
+                Sql &= "WHERE tsicod_1 BETWEEN '133' AND '195' AND "
+                Sql &= "      tsicod_2 <> ' ' AND "
+                Sql &= "      tsicod_3 = '304' AND "
+                Sql &= "      tsicod_4 <> ' ' "
+                Sql &= "ORDER BY tsicod_4, tsicod_2, tsicod_1"
+
+                da = New OracleDataAdapter(Sql, cn)
+                da.Fill(dt)
+
+                'Agrego las familias 2, 3, 4 y 5 a la tabla
+                For Each dr In dt.Rows
+                    dr1 = dtPpal.NewRow
+                    dr1("usr_0") = usr.Codigo
+                    dr1("tsicod1_0") = dr("tsicod_1")
+                    dr1("tsicod2_0") = dr("tsicod_2")
+                    dr1("tsicod3_0") = dr("tsicod_3")
+                    dr1("tsicod4_0") = dr("tsicod_4")
+                    dr1("qtydia_0") = 0
+                    dr1("predia_0") = 0
+                    dr1("impdia_0") = 0
+                    dr1("mardia_0") = 0
+                    dr1("qtyacu_0") = 0
+                    dr1("preacu_0") = 0
+                    dr1("impacu_0") = 0
+                    dr1("maracu_0") = 0
+                    dr1("qtypro_0") = 0
+                    dr1("imppro_0") = 0
+                    dr1("marpro_0") = 0
+                    dr1("qtyhis_0") = 0
+                    dr1("prehis_0") = 0
+                    dr1("imphis_0") = 0
+                    dr1("fecha_0") = Hasta.AddDays(-1)
+                    dtPpal.Rows.Add(dr1)
+                Next
+        End Select
 
     End Sub
     Private Sub ObtenerVentas()
@@ -203,6 +245,122 @@ Public Class ReporteVentaAnual
 
                 'Filtro la familia a la que voy a sumar las cantidades
                 dv.RowFilter = "tsicod3_0 = '" & dr("tsicod_3").ToString & "' AND tsicod4_0 = '" & dr("tsicod_4").ToString & "'"
+
+                If dv.Count = 1 Then 'Encontre la familia
+                    With dv.Item(0).Row
+                        .BeginEdit()
+                        'Calculo venta de año consultado
+                        If CDate(dr("accdat_0")).Year = AnoConsulta Then
+
+                            .Item("qtydia_0") = CDbl(.Item("qtydia_0")) + CDbl(dr("cant"))
+                            '.Item("predia_0") = CDbl(.Item("predia_0")) + CDbl(dr("punit"))
+                            .Item("impdia_0") = CDbl(.Item("impdia_0")) + CDbl(dr("impii"))
+
+                            .Item("qtyacu_0") = CDbl(.Item("qtyacu_0")) + CDbl(dr("cant"))
+                            '.Item("preacu_0") = CDbl(.Item("preacu_0")) + CDbl(dr("punit"))
+                            .Item("impacu_0") = CDbl(.Item("impacu_0")) + CDbl(dr("impii"))
+
+                            If Costos Then
+                                If itm.Abrir(dr("itmref_0").ToString) Then
+                                    Costo = CDbl(dr("cant")) * itm.Costo
+                                    .Item("mardia_0") = CDbl(.Item("mardia_0")) + Costo
+                                End If
+                            End If
+
+                        Else
+                            .Item("qtyhis_0") = CDbl(.Item("qtyhis_0")) + CDbl(dr("cant"))
+                            '.Item("prehis_0") = CDbl(.Item("prehis_0")) + CDbl(dr("punit"))
+                            .Item("imphis_0") = CDbl(.Item("imphis_0")) + CDbl(dr("impii"))
+
+                        End If
+                        .EndEdit()
+                    End With
+
+                End If
+
+            End If
+        Next
+
+        If Presupuesto Then
+            AplicarPresupuesto(Hasta)
+        End If
+
+        'Elimino registros sin cantidades
+        For i As Integer = dtPpal.Rows.Count - 1 To 0 Step -1
+            dr = dtPpal.Rows(i)
+
+            If dr.RowState <> DataRowState.Deleted Then
+
+                If CInt(dr("qtydia_0")) = 0 And CInt(dr("qtyacu_0")) = 0 And CInt(dr("qtyhis_0")) = 0 Then
+                    'Elimino registro sin cantidades
+                    dr.Delete()
+                End If
+
+            End If
+
+        Next
+
+    End Sub
+    Private Sub ObtenerVentasSF()
+        Dim Sql As String
+        Dim dt As New DataTable
+        Dim da As OracleDataAdapter
+        Dim dv As DataView
+        Dim dr As DataRow
+        Dim itm As New Articulo(cn)
+        Dim Costo As Double = 0
+
+        'Consulta para recuperar ventas del AÑO actual y del año anterior
+        Sql = "SELECT  qty_0 * sns_0 AS cant, "
+        Sql &= "       qty_0 * sns_0 * netpri_0 * ratcur_0 AS punit, "
+        Sql &= "       {amtatilin_0} * sns_0 * ratcur_0 AS impii, "
+        Sql &= "       itm.tsicod_1, "
+        Sql &= "       itm.tsicod_2, "
+        Sql &= "       itm.tsicod_3, "
+        Sql &= "       itm.tsicod_4, "
+        Sql &= "       accdat_0, "
+        Sql &= "       rep1_0, "
+        Sql &= "       sid.itmref_0 "
+        Sql &= "FROM sinvoice siv INNER JOIN "
+        Sql &= "     sinvoiced sid ON (siv.num_0 = sid.num_0) INNER JOIN "
+        Sql &= "     itmmaster itm ON (sid.itmref_0 = itm.itmref_0) "
+        Sql &= "WHERE accdat_0 >= :desde AND "
+        Sql &= "      accdat_0 < :hasta AND "
+        Sql &= "      sivtyp_0 <> 'PRF' "
+        Sql &= "ORDER BY accdat_0"
+
+        If Iva Then
+            Sql = Strings.Replace(Sql, "{amtatilin_0}", "amtatilin_0")
+        Else
+            Sql = Strings.Replace(Sql, "{amtatilin_0}", "amtnotlin_0")
+        End If
+
+        da = New OracleDataAdapter(Sql, cn)
+        da.SelectCommand.Parameters.Add("desde", OracleType.DateTime)
+        da.SelectCommand.Parameters.Add("hasta", OracleType.DateTime)
+
+        da.SelectCommand.Parameters("desde").Value = Desde
+        da.SelectCommand.Parameters("hasta").Value = Hasta
+        da.Fill(dt)
+
+        If Not Presupuesto Then
+            da.SelectCommand.Parameters("desde").Value = Desde.AddYears(-1)
+            da.SelectCommand.Parameters("hasta").Value = Hasta.AddYears(-1)
+            da.Fill(dt)
+        End If
+
+        dv = New DataView(dtPpal)
+
+        'CALCULO DE VENTAS DEL AÑO Y AÑO ANTERIOR
+        For Each dr In dt.Rows
+            'Proceso el registro si el vendedor está chequeado
+            If Vendedores.IndexOf(dr("rep1_0").ToString) > -1 Then
+
+                'Filtro la familia a la que voy a sumar las cantidades
+                dv.RowFilter = "tsicod1_0 = '" & dr("tsicod_1").ToString & "' AND " _
+                             & "tsicod2_0 = '" & dr("tsicod_2").ToString & "' AND " _
+                             & "tsicod3_0 = '" & dr("tsicod_3").ToString & "' AND " _
+                             & "tsicod4_0 = '" & dr("tsicod_4").ToString & "'"
 
                 If dv.Count = 1 Then 'Encontre la familia
                     With dv.Item(0).Row
